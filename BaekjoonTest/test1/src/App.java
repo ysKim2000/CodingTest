254badad905eb70f8501e27196ad7a40291077bc
Yunseo <kys051348@gmail.com>
2022-06-26 01:16:59 +0900

    test2

--- BaekjoonTest/test1/src/App.java
+++ BaekjoonTest/test1/src/App.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String sNum = sc.next();
+        int sum = 0;
+
+        for (int i = 0; i < num; i++) {
+            sum += sNum.charAt(i) - '0';
+        }
+        System.out.println(sum);
+        sc.close();
     }
 }
---
+++ BaekjoonTest/test2/test2/src/App.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int score[] = new int[num];
+        int sum = 0, max = 0;
+
+        for(int i=0; i<num;i++){
+            score[i] = sc.nextInt();
+            if(score[i] > max) max = score[i];
+            sum += score[i];
+        }
+        System.out.println(sum * 100.0 / max / num);
+        sc.close();
+    }
+}
