60b5dfb60e74dd8382e120051ad3e30e0f28bcfd
Yunseo Kim <kys051348@gmail.com>
2022-07-01 20:27:09 +0900

    test13

---
+++ BaekjoonTest/test13/src/Test13.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            queue.add(i + 1);
+        }
+        while(queue.size() > 1){
+            queue.remove();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.peek());
+    }
+}
