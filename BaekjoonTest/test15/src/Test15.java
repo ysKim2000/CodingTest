de9d1e9eeae76918e8abee7de2d21abaacbd76f6
Yunseo Kim <kys051348@gmail.com>
2022-07-02 23:26:04 +0900

    test15

---
+++ BaekjoonTest/test15/src/Test15.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        for (int i = 0; i < N - 1; i++) {
+            int tmp = 0;
+            for (int j = 0; j < N - i - 1; j++) {
+                if (A[j] > A[j + 1]) {
+                    tmp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
