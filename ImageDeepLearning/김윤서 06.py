f7d8fbdd1162deb8d9d2973b64951a67de62f6d2
Yunseo <kys051348@gmail.com>
2022-09-14 11:27:00 +0900

    6th

---
+++ ImageDeepLearning/김윤서 06.py
@@ -0,0 +1,29 @@
+# 2018번
+# 연속된 자연수의 합
+
+import sys
+input = sys.stdin.readline
+
+
+def solve():
+    N = int(input())
+    cnt = 1
+    sum = 0
+    start = 0
+    end = 0
+    while end <= N / 2 + 1:
+        if sum < N:
+            end += 1
+            sum += end
+        elif sum > N:
+            sum -= start
+            start += 1
+        else:
+            cnt += 1
+            end += 1
+            sum += end
+    print(cnt)
+
+
+if __name__ == "__main__":
+    solve()
