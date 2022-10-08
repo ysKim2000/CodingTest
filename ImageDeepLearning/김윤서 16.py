2eacf58b6188c7d678b97b007d6382db32e6c959
Yunseo <kys051348@gmail.com>
2022-10-08 12:29:35 +0900

    .

--- ImageDeepLearning/김윤서 16.py
+++ ImageDeepLearning/김윤서 16.py
@@ -6,10 +6,6 @@ input = sys.stdin.readline
 
 def solution_16(nums):
     cnt = 0
-    # for i in range(len(nums)):
-    #     for j in range(len(nums)):
-    #         if nums[i] < nums[j]:
-    #             nums[i], nums[j] = nums[j], nums[i]
 
     nums = sorted(nums)
     for i in range(len(nums)) :
---
+++ ImageDeepLearning/김윤서 20.py
@@ -0,0 +1,39 @@
+# 2751번
+
+import sys
+input = sys.stdin.readline
+
+
+def solution_20(nums):
+    nums = merge_sort(nums)
+    for i in nums:
+        print(i)
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+
+    merged_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] < high_arr[h]:
+            merged_arr.append(low_arr[l])
+            l += 1
+        else:
+            merged_arr.append(high_arr[h])
+            h += 1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+    return merged_arr
+
+
+N = int(input())
+nums = []
+for i in range(N):
+    nums.append(int(input()))
+
+solution_20(nums)
